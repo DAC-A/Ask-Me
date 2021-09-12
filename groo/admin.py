98f000a64e633cc8598f25b01d254f2ef12e4390
Asmit Pandey <79566317+asmit0701@users.noreply.github.com>
2021-09-13 01:04:10 +0530

    Signup backend completed

--- groo/admin.py
+++ groo/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from groo.models import NewUser
 
-# Register your models here.
+admin.site.register(NewUser)
--- groo/urls.py
+++ groo/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from groo import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('signup',views.signup,name='signup'),
+    path('formsignup',views.formsignup,name='formsignup'),
 ]
